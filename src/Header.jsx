d8f855fc101c5005624f9a86003051711caa238c
aaron1986 <aaronbrucesmith86@gmail.com>
2023-10-24 18:21:15 +0100

    css addded

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
         <nav>
         <ul>
         <li><a href="">Home</a></li>
-        <li><a href="">Json</a></li>
-        <li><a href="">GitHub</a></li>
+        <li><a href="https://codefellows.github.io/code-301-guide/curriculum/class-02/lab/assets/data.json" target="_blank">Json</a></li>
+        <li><a href="https://github.com/aaron1986/Lab-Class-02-Week-Five" target="_blank">GitHub</a></li>
         </ul>
         </nav>
         </header>
